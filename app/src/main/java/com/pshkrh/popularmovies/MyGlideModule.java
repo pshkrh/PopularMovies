2fb9738f637d3d293d9ee80cb7ca6a3891e3c0a8
Pushkar Kurhekar <pshkrh@gmail.com>
2018-08-09 16:53:50 +0530

    Add Glide Module

---
+++ app/src/main/java/com/pshkrh/popularmovies/MyGlideModule.java
@@ -0,0 +1,7 @@
+package com.pshkrh.popularmovies;
+
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.annotation.GlideModule;
+
+@GlideModule
+public final class MyGlideModule extends AppGlideModule {}
