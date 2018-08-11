4dd781a6e2006c0437f88ece589b525ccf2cf9aa
Pushkar Kurhekar <pshkrh@gmail.com>
2018-08-11 15:53:25 +0530

    MainActivity now handles both Movies and Favourites

--- app/src/main/java/com/pshkrh/popularmovies/MainActivity.java
+++ app/src/main/java/com/pshkrh/popularmovies/MainActivity.java
@@ -2,9 +2,18 @@ package com.pshkrh.popularmovies;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -28,7 +37,8 @@ import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
 
     public static String TAG = "MainActivity";
 
@@ -45,6 +55,12 @@ public class MainActivity extends AppCompatActivity {
     private Parcelable mRecyclerState;
 
     private RecyclerView recyclerView;
+    private final static String FAVOURITES_KEY = "favouritesKey";
+    public static final int LOADER_ID = 0;
+
+    public static String SORT_FLAG = "mp";
+
+    FavouritesAdapter favouritesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this,noOfColumns));
 
+        favouritesAdapter = new FavouritesAdapter(mFavourites,this);
+
         mMovies = new ArrayList<>();
         mFavourites = new ArrayList<>();
 
@@ -65,6 +83,14 @@ public class MainActivity extends AppCompatActivity {
             MovieAdapter movieAdapter = new MovieAdapter(mMovies);
             recyclerView.setAdapter(movieAdapter);
             movieAdapter.notifyDataSetChanged();
+            progressBar.setVisibility(View.INVISIBLE);
+            recyclerView.setVisibility(View.VISIBLE);
+        }
+        else if(savedInstanceState != null && savedInstanceState.containsKey(FAVOURITES_KEY)){
+            mFavourites = savedInstanceState.getParcelableArrayList(FAVOURITES_KEY);
+            favouritesAdapter = new FavouritesAdapter(mFavourites,this);
+            recyclerView.setAdapter(favouritesAdapter);
+            favouritesAdapter.notifyDataSetChanged();
             progressBar.setVisibility(View.INVISIBLE);
             recyclerView.setVisibility(View.VISIBLE);
         }
@@ -84,14 +110,16 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.sort_most_popular:
+                SORT_FLAG = "mp";
                 networkingTime(MOVIE_POPULAR_BASE_URL);
                 return true;
             case R.id.sort_top_rated:
+                SORT_FLAG = "top";
                 networkingTime(MOVIE_TOP_RATED_BASE_URL);
                 return true;
             case R.id.sort_favourite:
-                Intent intent = new Intent(MainActivity.this,FavouritesActivity.class);
-                startActivity(intent);
+                SORT_FLAG = "fav";
+                getFavouriteMovies();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -167,6 +195,8 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerState = recyclerView.getLayoutManager().onSaveInstanceState();
         outState.putParcelable(RECYCLER_STATE_KEY,mRecyclerState);
         outState.putParcelableArrayList(MOVIES_KEY,mMovies);
+        outState.putParcelableArrayList(FAVOURITES_KEY,mFavourites);
+        mFavourites.clear();
     }
 
     @Override
@@ -175,14 +205,98 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerState = savedInstanceState.getParcelable(RECYCLER_STATE_KEY);
         mMovies = savedInstanceState.getParcelableArrayList(MOVIES_KEY);
+        mFavourites = savedInstanceState.getParcelableArrayList(FAVOURITES_KEY);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        if(SORT_FLAG.equals("fav")){
+            mFavourites.clear();
+            getFavouriteMovies();
+        }
         if(mRecyclerState!=null){
             recyclerView.getLayoutManager().onRestoreInstanceState(mRecyclerState);
         }
     }
+
+    private void getFavouriteMovies(){
+        Uri uri = DBContract.DBEntry.CONTENT_URI;
+        Cursor mCursor = getContentResolver().query(uri,null,null,null,null);
+        if(mCursor!=null && mCursor.getCount() > 0){
+            mCursor.moveToFirst();
+            do{
+                String name = mCursor.getString(1);
+                String rating = mCursor.getString(2);
+                String overview = mCursor.getString(3);
+                String date = mCursor.getString(4);
+                byte[] byteArray = mCursor.getBlob(5);
+                Bitmap bm = BitmapFactory.decodeByteArray(byteArray, 0 ,byteArray.length);
+                String id = mCursor.getString(6);
+
+                String check = name + "///" + rating + "///" + id;
+                Log.d(TAG,check);
+                Favourite favourite = new Favourite(name,overview,rating,date,id,bm);
+                mFavourites.add(favourite);
+            } while(mCursor.moveToNext());
+            mCursor.close();
+            RecyclerView recyclerView = findViewById(R.id.recycler);
+            FavouritesAdapter favouritesAdapter = new FavouritesAdapter(mFavourites,this);
+            recyclerView.setAdapter(favouritesAdapter);
+            favouritesAdapter.notifyDataSetChanged();
+            recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this,noOfColumns));
+        }
+        else{
+            Toast.makeText(this, "No Favourite Movies Yet!", Toast.LENGTH_SHORT).show();
+            SORT_FLAG = "mp";
+        }
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new AsyncTaskLoader<Cursor>(this) {
+
+            Cursor mFavouritesData = null;
+
+            @Override
+            protected void onStartLoading() {
+                if (mFavouritesData != null) {
+                    deliverResult(mFavouritesData);
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public Cursor loadInBackground() {
+                try{
+                    return getContentResolver().query(DBContract.DBEntry.CONTENT_URI,
+                            null,null,null,null);
+                } catch(Exception e){
+                    Log.e(TAG,"Failed to asynchronously load data.");
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            public void deliverResult(Cursor data) {
+                mFavouritesData = data;
+                super.deliverResult(data);
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+
 }
